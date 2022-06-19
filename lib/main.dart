fed9f047f6a6f28ce93aab27d3f519d9379cf8e4
Shammas-mk <mkshammas00@gmail.com>
2022-06-20 02:22:19 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:pushnotification/homepage.dart';
+import 'package:pushnotification/services/local_notification.dart';
 
-
-Future <void> 
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.title);
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await FireBase.initializeApp();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+  LocalNotificationService.initialize();
   runApp(const MyApp());
 }
 
--- lib/services/local_notification.dart
+++ lib/services/local_notification.dart
@@ -1,1 +1,48 @@
-class LocalNotificationService {}
+import 'dart:developer';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  static void initialize() {
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: AndroidInitializationSettings("@tpmap/ic_launcher"),
+    );
+    _notificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: (String? id) async {
+        print("onSelectNotification");
+        if (id!.isEmpty) {
+          print("Router+++++++++++++++++++++++++++++$id");
+        }
+      },
+    );
+  }
+
+  static void createAndDisplayNotification(RemoteMessage message) async {
+    try {
+      final id = DateTime.now().millisecondsSinceEpoch ~/ 1088;
+
+      const NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails(
+          "",
+          "",
+          importance: Importance.max,
+          priority: Priority.high,
+        ),
+      );
+      await _notificationsPlugin.show(
+        id,
+        message.notification!.title,
+        message.notification!.body,
+        notificationDetails,
+        payload: message.data["id"],
+      );
+    } on Exception catch (e) {
+      print(e);
+    }
+  }
+}
